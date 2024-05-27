684b6e3ba8ba6bc586b7ee39268879ceeed3995c
ArvyTech <rikshitvashishtha111@gmail.com>
2024-05-27 12:16:39 +0530

    setup AI in React

--- react-ai/src/App.js
+++ react-ai/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios';
+
+import CGlogo from './CGlogo.png'; 
 import './App.css';
 
 function App() {
+  const {prompt, setPrompt} = useState('');
+  const {response, setResponse} = useState('');
+  const {loading, setLoading} = useState(false);
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      setLoading(true);
+
+      axios.post("http://localhost:3000/chat", {prompt})
+      .then((res) => {
+        setResponse(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <form onSubmit={handleSubmit}>
+            <img src={CGlogo} alt='' className={loading ? 'cg-logo loading' : 'cg-logo'} />
+            <input 
+              type = "text"
+              value = {prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder = "Ask anything... :)"
+            />
+            <button type="submit">Ask</button>
+         </form>
+
+         <p className="responserArea">
+            {loading ? 'loading..' : response }
+            {/* Here will be response from AI... */}
+         </p>
+
+         <div className="footer">~ Rikshit-Development ~</div>
     </div>
   );
 }
